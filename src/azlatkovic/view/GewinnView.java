4e2d8f9b5db98386e03795e3519f999457568a03
andjelaa-z <andjeladevojcica@gmail.com>
2026-09-23 16:30:18 +0200

    2 neue Attribute in View und die Labels zur Anzeige der Punkte ergänzt.

--- src/azlatkovic/view/GewinnView.java
+++ src/azlatkovic/view/GewinnView.java
@@ -9,9 +9,48 @@ public class GewinnView extends JFrame{
     private JTextField spielerZahl;
     private JTextField computerZahl;
 
+    private JLabel rundenueberschrift;
+    private JLabel gesamtueberschrift;
+
     private JButton nochEinmal;
 
-public GewinnView(){
+    public GewinnView(){
+        setTitle("Zahlen-Gewinnspiel (v1.0)");
+        setSize(400, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-}
+        JPanel northPanel = new JPanel(new GridLayout(1, 2));
+        JPanel rundenPanel = new JPanel(new GridLayout(2, 1));
+        JPanel gesamtPanel = new JPanel(new GridLayout(2, 1));
+        northPanel.add(rundenPanel);
+        northPanel.add(gesamtPanel);
+
+        rundenueberschrift = new JLabel("Rundenergebnis: ");
+        rundenPanel.add(rundenueberschrift);
+        rundenueberschrift.setHorizontalAlignment(SwingConstants.CENTER);
+
+        gesamtueberschrift = new JLabel("Gesamtpunkte: ");
+        gesamtPanel.add(gesamtueberschrift);
+        gesamtueberschrift.setHorizontalAlignment(SwingConstants.CENTER);
+
+        rundenergebnis = new JLabel("");
+        rundenergebnis.setOpaque(true);
+        rundenergebnis.setBackground(Color.WHITE);
+        rundenPanel.add(rundenergebnis);
+
+        gesamtPunkte = new JLabel("");
+        gesamtPunkte.setOpaque(true);
+        gesamtPunkte.setBackground(Color.WHITE);
+        gesamtPanel.add(gesamtPunkte);
+
+        add(northPanel, BorderLayout.NORTH);
+
+
+
+
+
+
+
+    }
 }
