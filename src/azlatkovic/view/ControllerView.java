4b4dec982fc59b4f681b19b955d150031e131418
andjelaa-z <andjeladevojcica@gmail.com>
2026-09-23 16:05:26 +0200

    View Klasse erstellt und Attribute / Komponente deklariert.

---
+++ src/azlatkovic/view/ControllerView.java
@@ -0,0 +1,15 @@
+package azlatkovic.view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class ControllerView extends JFrame{
+    private JLabel gesamtPunkte;
+    private JLabel rundenergebnis;
+    private JTextField spielerZahl;
+    private JTextField computerZahl;
+
+    private JButton nochEinmal;
+
+
+}
