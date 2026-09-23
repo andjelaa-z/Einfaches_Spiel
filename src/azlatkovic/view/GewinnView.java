24caed9bb66195f83e2ea28716ed0e1ff31a66dd
andjelaa-z <andjeladevojcica@gmail.com>
2026-09-23 18:52:29 +0200

    Das Label wird Rot wenn Punkte verloren werden und Grün wenn gewonnen und Wenn die Gesamtpunkte über 100 sind dann steht gewonnen da und wenn unter 0 dann verloren.

--- src/azlatkovic/view/GewinnView.java
+++ src/azlatkovic/view/GewinnView.java
@@ -105,8 +105,13 @@ public class GewinnView extends JFrame{
         computerZahl.setText(String.valueOf(zahl));
     }
 
-    public void setRundenergebnis(int ergebnis){
-        if(ergebnis == 20){
+    public void setRundenergebnis(int ergebnis, boolean gewonnen, boolean verloren){
+        if(gewonnen){
+            rundenergebnis.setText("Gewonnen!");
+        }else if(verloren){
+            rundenergebnis.setText("Verloren!");
+        }
+        else if(ergebnis == 20){
             rundenergebnis.setText("+20");
         }
         else if(ergebnis == 5){
@@ -125,6 +130,20 @@ public class GewinnView extends JFrame{
         spielerZahl.setText("");
         computerZahl.setText("");
         rundenergebnis.setText("Tippe eine Zahl von 1 bis 9");
+    }
+
+    public void setFarben(int ergebnis, boolean gewonnen, boolean verloren){
+        if(gewonnen || ergebnis > 0){
+            rundenergebnis.setBackground(Color.GREEN);
+            gesamtPunkte.setBackground(Color.GREEN);
+        }else if(verloren || ergebnis < 0){
+            rundenergebnis.setBackground(Color.RED);
+            gesamtPunkte.setBackground(Color.RED);
+        }else{
+            rundenergebnis.setBackground(Color.WHITE);
+            gesamtPunkte.setBackground(Color.WHITE);
+        }
+
     }
 
 }
