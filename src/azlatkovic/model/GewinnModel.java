a24dd9b3b7b613921e4132e54fe2e081181ba908
andjelaa-z <andjeladevojcica@gmail.com>
2026-09-23 15:53:21 +0200

    Model Klasse mit Attributen und Konstruktor, der die Startpunkte setzt, erstellt.

---
+++ src/azlatkovic/model/GewinnModel.java
@@ -0,0 +1,12 @@
+package azlatkovic.model;
+
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel(){
+        gesamtPunkte = 30;
+    }
+}
